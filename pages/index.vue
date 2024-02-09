cadb19424916c2f098f16e1b14a2c144f26eb172
MahardiikaKD <dikamahar884@gmail.com>
2024-02-10 02:40:36 +0700

    add meta solution

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,22 @@
 
 <script lang="ts" setup>
 definePageMeta({
-    name: 'My home page',
-    layout: 'default',
-})
+	layout: 'default',
+});
+const pageTitleContext = 'It Solution - Landing Page';
+const pageDescriptionContext =
+	'Solusi IT terkini untuk meningkatkan produktivitas bisnis Anda.';
+
+// Mengisi metadata berdasarkan konteks
+useHead(() => ({
+	title: pageTitleContext,
+	meta: [
+		{
+			hid: 'description',
+			name: 'description',
+			content: pageDescriptionContext,
+		},
+		// Anda dapat menambahkan meta tag lainnya sesuai kebutuhan
+	],
+}));
 </script>
