23b4544a6fb3754e4b8afd9edd073f515d281d77
MahardiikaKD <dikamahar884@gmail.com>
2024-02-10 20:26:52 +0700

    seals

--- components/Seals.vue
+++ components/Seals.vue
@@ -1,20 +1,37 @@
 <template>
-	<section class="py-[20px] px-2 sm:relative sm:top-[-118px]">
+	<section class="py-[20px] px-2 sm:relative sm:top-[-68px]">
 		<div class="z-10 sm:py-6 px-2 sm:px-36">
 			<div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
 				<div
 					v-for="(item, index) in sealsItems"
 					:key="index"
-					class="border p-6 rounded">
-					<div class="flex gap-3 bg-opacity-10 bg-[#ffffff1A]">
-						<icon :name="item?.icon" class="text-[30px] sm:text-white font-bold" />
-						<span class="items-center flex text-sm sm:text-white font-bold">
+					class="border p-6 rounded bg-white shadow-lg seals-hover">
+					<div
+						class="flex gap-3 transition duration-300">
+						<icon
+                            id="icon"
+							:name="item?.icon"
+							class="text-[30px] sm:text-slate-900 font-bold" 
+                        />
+						<span
+                            id="title"
+							class="items-center flex text-sm sm:text-slate-900 font-bold">
 							{{ item?.title }}
 						</span>
 					</div>
 					<div class="mt-2 h-[100px] p-3 flex items-center">
-						{{ item?.description }}
+                        <div
+                            id="description"
+                            class="text-slate-900"
+                        >
+                            {{ item?.description }}
+                        </div>
 					</div>
+                    <div class="flex justify-end">
+                        <button class="border rounded-md px-10 border-slate-900">
+                            <icon id="right-icon" name="icon-park-outline:right-two" class="text-sm text-slate-900" />
+                        </button>
+                    </div>
 				</div>
 			</div>
 		</div>
@@ -58,5 +75,45 @@ const sealsItems = ref<SealsItems[]>([
 .fade-enter,
 .fade-leave-to {
 	opacity: 0;
+}
+
+/* Tambahkan aturan CSS berikut */
+.bg-opacity-80 {
+	background-color: rgba(255, 255, 255, 0.8);
+}
+
+.hover\:bg-opacity-100:hover {
+	background-color: rgba(255, 255, 255, 1);
+}
+
+/* Penyesuaian warna teks untuk kontras */
+.text-white {
+	color: white;
+}
+
+.seals-hover {
+    &:hover {
+        @apply border-green-100 bg-green-50 cursor-pointer;
+
+        #title {
+            @apply text-green-500;
+        }
+
+        #icon {
+            @apply text-green-500;
+        }
+
+        #description {
+            @apply text-green-500;
+        }
+
+        button {
+            @apply border-green-500 bg-green-50;
+        }
+
+        #right-icon {
+            @apply text-green-500;
+        }
+    }
 }
 </style>
