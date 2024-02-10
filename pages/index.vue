4cc355fca1b9d513179f86f83f84bca4bc2e27cd
MahardiikaKD <dikamahar884@gmail.com>
2024-02-10 13:54:44 +0700

    meta data

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
-        <hero-section />
-    </div>
+		<hero-section />
+	</div>
 </template>
 
 <script lang="ts" setup>
@@ -20,6 +20,51 @@ useHead(() => ({
 			hid: 'description',
 			name: 'description',
 			content: pageDescriptionContext,
+		},
+		{
+			hid: 'keywords',
+			name: 'keywords',
+			content: 'kata kunci, lainnya, disini',
+		},
+		{
+			hid: 'og:title',
+			property: 'og:title',
+			content: pageTitleContext,
+		},
+		{
+			hid: 'og:description',
+			property: 'og:description',
+			content: pageDescriptionContext,
+		},
+		{
+			hid: 'og:image',
+			property: 'og:image',
+			content: 'https://oitech-nuxt.vercel.app/images/logo-2.png',
+		},
+		{
+			hid: 'og:url',
+			property: 'og:url',
+			content: 'https://oitech-nuxt.vercel.app/images/logo-2.png',
+		},
+		{
+			hid: 'twitter:title',
+			name: 'twitter:title',
+			content: pageTitleContext,
+		},
+		{
+			hid: 'twitter:description',
+			name: 'twitter:description',
+			content: pageDescriptionContext,
+		},
+		{
+			hid: 'twitter:image',
+			name: 'twitter:image',
+			content: 'https://oitech-nuxt.vercel.app/images/logo-2.png',
+		},
+		{
+			hid: 'twitter:card',
+			name: 'twitter:card',
+			content: 'Haloo',
 		},
 	],
 }));
